2523b95cb94df9257266d5d072f9a80158fd3808
hammackjg <hammackjg@g.crookcountyschools.org>
2022-02-28 09:57:21 -0800

    modified index.html and sales.html and sales.js and style.css

--- sales.js
+++ sales.js
@@ -1,138 +1,369 @@
 const hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+
+
+
+
 const tableElement = document.getElementById('sales-table');
- 
-// TODO:  create a comment below describing how this Constructor function works
-// - what makes it a constructor function?
-// constructor function allows you to call stuff with this easier than object array thing i guess
-// - what do you think the CookieStand.all.push(all) method is doing?
- 
+
+
+
+
+
+
+
+// TODO: pass minCustomersPerHour, maxCustomersPerHour, and avgCookiesPerHour as parameters of this function
+
+
+// set them as dynamic properties inside of this (based on their passed in data)
+
+
 function CookieStand(locationName, minCustomersPerHour, maxCustomersPerHour, avgCookiesPerSale) {
-this.locationName = locationName;
-this.minCustomersPerHour = minCustomersPerHour;
-this.maxCustomersPerHour = maxCustomersPerHour;
-this.avgCookiesPerSale = avgCookiesPerSale;
 
-this.customersEachHour = [];
-this.cookiesEachHour = [];
-this.totalDailyCookies = 0;
-CookieStand.all.push(this);
+
+ this.locationName = locationName;
+
+
+ this.customersEachHour = [];
+
+
+ this.cookiesEachHour = [];
+
+
+ this.totalDailyCookies = 0;
+
+
+ // TODO: // push "this" object to the CookieStand.all array on every instantiation
+
+
 }
-// TODO: create a comment describing the method below
-// - what does prototype mean?
-// Prototypes add soemthing to your fuction that you can call specifically that isn't in your original function 
- 
+
+
+
+
+
+
+
 CookieStand.prototype.calcCustomersEachHour = function() {
-for (let i = 0; i < hours.length; i++) {
-  this.customersEachHour.push(random(this.minCustomersPerHour, this.maxCustomersPerHour));
-}
+
+
+ for (let i = 0; i < hours.length; i++) {
+
+
+   this.customersEachHour.push(random(this.minCustomersPerHour, this.maxCustomersPerHour));
+
+
+ }
+
+
 };
+
+
+
+
+
+
+
 CookieStand.prototype.calcCookiesEachHour = function() {
-this.calcCustomersEachHour();
-for (let i = 0; i < hours.length; i++) {
- const oneHour = Math.ceil(this.customersEachHour[i] * this.avgCookiesPerSale);
-  this.cookiesEachHour.push(oneHour);
-  this.totalDailyCookies += oneHour;
-}
+
+
+ // TODO: invoke the method that calculates number of customers each hour here
+
+
+ for (let i = 0; i < hours.length; i++) {
+
+
+   const oneHour = Math.ceil(this.customersEachHour[i] * this.avgCookiesPerSale);
+
+
+   this.cookiesEachHour.push(oneHour);
+
+
+   this.totalDailyCookies += oneHour;
+
+
+ }
+
+
 };
+
+
+
+
+
+
+
 CookieStand.prototype.render = function() {
-this.calcCookiesEachHour();
-const tableRow = document.createElement('tr');
-let tableDataElement = document.createElement('td');
-tableDataElement.textContent = this.locationName;
-tableRow.appendChild(tableDataElement);
-for (let i = 0; i < hours.length; i++) {
-  tableDataElement = document.createElement('td');
-  tableDataElement.textContent = this.cookiesEachHour[i];
-  tableRow.appendChild(tableDataElement);
-}
-const tableHeaderElement = document.createElement('th');
-tableHeaderElement.textContent = this.totalDailyCookies;
-tableRow.appendChild(tableHeaderElement);
-tableElement.appendChild(tableRow);
+
+
+ // TODO: generate the calculation of customers each hour here
+
+
+ const tableRow = document.createElement('tr');
+
+
+ let tableDataElement = document.createElement('td');
+
+
+ tableDataElement.textContent = this.locationName;
+
+
+ tableRow.appendChild(tableDataElement);
+
+
+ for (let i = 0; i < hours.length; i++) {
+
+
+   tableDataElement = document.createElement('td');
+
+
+   tableDataElement.textContent = this.cookiesEachHour[i];
+
+
+   tableRow.appendChild(tableDataElement);
+
+
+ }
+
+
+ const tableHeaderElement = document.createElement('th');
+
+
+ tableHeaderElement.textContent = this.totalDailyCookies;
+
+
+ tableRow.appendChild(tableHeaderElement);
+
+
+ tableElement.appendChild(tableRow);
+
+
 };
+
+
+
+
+
+
+
 CookieStand.all = [];
-// TODO: instantiate a new CookieStand object (with sample data) for Dubai, Paris, and Lima
 
-new CookieStand('Seattle', 23, 65, 6.3);
- 
-new CookieStand('Tokyo', 3, 24, 1.2);
 
-new CookieStand('Dubai', 16, 42, 5.3);
 
-new CookieStand('Paris', 7, 24, 2.3);
 
-new CookieStand('Lima', 13, 53, 4.7);
- 
+
+
+
+// TODO: instantiate all CookieStand objects (with sample data)
+
+
+
+
+
+
+
 function random(min, max) {
-return Math.floor(Math.random() * (max - min + 1)) + min;
-}
- 
-function makeHeaderRow() {
-const tableRow = document.createElement('tr');
-let tableHeaderElement = document.createElement('th');
-tableHeaderElement.textContent = 'Locations';
-tableRow.appendChild(tableHeaderElement);
 
-for (let i = 0; i < hours.length; i++) {
-  tableHeaderElement = document.createElement('th');
-  tableHeaderElement.textContent = hours[i];
-  tableRow.appendChild(tableHeaderElement);
+
+ return Math.floor(Math.random() * (max - min + 1)) + min;
+
+
 }
-tableHeaderElement = document.createElement('th');
-tableHeaderElement.textContent = 'Location Totals';
-tableRow.appendChild(tableHeaderElement);
-tableElement.appendChild(tableRow);
+
+
+
+
+
+
+
+function makeHeaderRow() {
+
+
+ const tableRow = document.createElement('tr');
+
+
+ let tableHeaderElement = document.createElement('th');
+
+
+ tableHeaderElement.textContent = 'Locations';
+
+
+ tableRow.appendChild(tableHeaderElement);
+
+
+ for (let i = 0; i < hours.length; i++) {
+
+
+   tableHeaderElement = document.createElement('th');
+
+
+   tableHeaderElement.textContent = hours[i];
+
+
+   tableRow.appendChild(tableHeaderElement);
+
+
+ }
+
+
+ tableHeaderElement = document.createElement('th');
+
+
+ tableHeaderElement.textContent = 'Location Totals';
+
+
+ tableRow.appendChild(tableHeaderElement);
+
+
+ tableElement.appendChild(tableRow);
+
+
 }
-// TODO: create a series of comments in this function where you are confused with what's happening
-// - what about the code is confusing?
-// The two loops is kind of confusing.
-// - can you guess what it does?
-// I think one loops through everything and the other loops through all of the totals to count them all
+
+
+
+
+
+
 
 function makeFooterRow() {
 
-  const tableRow = document.createElement('tr');
-let tableHeaderElement = document.createElement('th');
-tableHeaderElement.textContent = 'Hourly Totals for All Locations';
-tableRow.appendChild(tableHeaderElement);
-let totalOfTotals = 0;
 
-for (let i = 0; i < hours.length; i++) {
-  let hourlyTotal = 0;
-  
-  for (let j = 0; j < CookieStand.all.length; j++){
-    hourlyTotal += CookieStand.all[j].cookiesEachHour[i];
-    totalOfTotals += CookieStand.all[j].cookiesEachHour[i];
-  }
- 
-  tableHeaderElement = document.createElement('th');
-  tableHeaderElement.textContent = hourlyTotal;
-  tableRow.appendChild(tableHeaderElement);
+ const tableRow = document.createElement('tr');
+
+
+ let tableHeaderElement = document.createElement('th');
+
+
+ tableHeaderElement.textContent = 'Hourly Totals for All Locations';
+
+
+ tableRow.appendChild(tableHeaderElement);
+
+
+ let totalOfTotals = 0;
+
+
+ for (let i = 0; i < hours.length; i++) {
+
+
+   let hourlyTotal = 0;
+
+
+   for (const j = 0; j < CookieStand.all.length; j++){
+
+
+     hourlyTotal += CookieStand.all[j].cookiesEachHour[i];
+
+
+     totalOfTotals += CookieStand.all[j].cookiesEachHour[i];
+
+
+   }
+
+
+   tableHeaderElement = document.createElement('th');
+
+
+   tableHeaderElement.textContent = hourlyTotal;
+
+
+   tableRow.appendChild(tableHeaderElement);
+
+
+ }
+
+
+ tableHeaderElement = document.createElement('th');
+
+
+ tableHeaderElement.textContent = totalOfTotals;
+
+
+ tableRow.appendChild(tableHeaderElement);
+
+
+ tableElement.appendChild(tableRow);
+
+
 }
-tableHeaderElement = document.createElement('th');
-tableHeaderElement.textContent = totalOfTotals;
-tableRow.appendChild(tableHeaderElement);
-tableElement.appendChild(tableRow);
-}
+
+
+
+
+
+
+
 (function renderTable() {
-makeHeaderRow();
-for(let i = 0; i < CookieStand.all.length; i++){
-  CookieStand.all[i].render();
-}
-makeFooterRow();
+
+
+ makeHeaderRow();
+
+
+ for(let i = 0; i < CookieStand.all.length; i++){
+
+
+   CookieStand.all[i].render();
+
+
+ }
+
+
+ makeFooterRow();
+
+
 })();
- 
-// const ocean = document.getElementById('ocean'),
-// waveWidth = 10,
-// waveCount = Math.floor(window.innerWidth/waveWidth),
-// docFrag = document.createDocumentFragment();
-// for(let i = 0; i < waveCount; i++){
-// const wave = document.createElement('div');
-// wave.className += ' wave';
-// docFrag.appendChild(wave);
-// wave.style.left = i * waveWidth + 'px';
-// wave.style.webkitAnimationDelay = (i/100) + 's';
-// }
-// // TODO: put docFrag as the argument to appendChild below
-// ocean.appendChild(docFrag);
-//   <script src="js/app.js"></script>
+
+
+
+
+
+
+
+// make some waves.
+
+
+const ocean = document.getElementById('ocean'),
+
+
+ waveWidth = 10,
+
+
+ waveCount = Math.floor(window.innerWidth/waveWidth),
+
+
+ docFrag = document.createDocumentFragment();
+
+
+
+
+
+
+
+for(let i = 0; i < waveCount; i++){
+
+
+ const wave = document.createElement('div');
+
+
+ wave.className += ' wave';
+
+
+ docFrag.appendChild(wave);
+
+
+ wave.style.left = i * waveWidth + 'px';
+
+
+ wave.style.webkitAnimationDelay = (i/100) + 's';
+
+
+}
+
+
+
+
+
+
+
+ocean.appendChild(docFrag);
+
