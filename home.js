fe03f494b04ef744d173cfb03916e9d02e35f01e
hammackjg <hammackjg@g.crookcountyschools.org>
2022-03-04 12:54:29 -0800

    modified home.css and modified home.js and modifed index.html

--- home.js
+++ home.js
@@ -1,18 +1,38 @@
 'use strict'
 
-let slideIndex = 0;
-carousel();
-function carousel() {
- let i;
- let fishImgs = document.getElementsByClassName('carouselImg');
- for (i = 0; i < fishImgs.length; i++) {
-   fishImgs[i].style.display = 'none';
- }
- slideIndex++;
- if (slideIndex > fishImgs.length) { slideIndex = 1; }
- fishImgs[slideIndex - 1].style.display = 'block';
- setTimeout(carousel, 2000);
+let slides = document.getElementsByClassName("slider__slide");
+let navlinks = document.getElementsByClassName("slider__navlink");
+
+let currentSlide = 0;
+
+document.getElementById("nav-button--next").addEventListener("click", () => {
+  changeSlide(currentSlide + 1)
+});
+document.getElementById("nav-button--prev").addEventListener("click", () => {
+  changeSlide(currentSlide - 1)
+});
+
+document.getElementById("nav-button--next").addEventListener("click", changeSlide(currentSlide + 1));
+
+function changeSlide(moveTo) {
+  if (moveTo >= slides.length) {moveTo = 0;}
+  if (moveTo < 0) {moveTo = slides.length - 1;}
+  
+  slides[currentSlide].classList.toggle("active");
+  navlinks[currentSlide].classList.toggle("active");
+  slides[moveTo].classList.toggle("active");
+  navlinks[moveTo].classList.toggle("active");
+  
+  currentSlide = moveTo;
 }
+
+document.querySelectorAll('.slider__navlink').forEach((bullet, bulletIndex) => {
+  bullet.addEventListener('click', () => {
+      if (currentSlide !== bulletIndex) {
+          changeSlide(bulletIndex);
+      }
+  })
+})
 
 
 
